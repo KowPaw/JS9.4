08e24b8a6cff4773eba019d9918a89fa28a0bea4
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2017-12-01 11:15:42 +0100

    Change in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
 // scripts.js
 
+// half Chrismas tree
 function drawTree(h) {
+	for (i = 1; i <= h; i++) {
+		var star = '';
+		for (j = 0; j < i; j++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+}
+
+drawTree(9);
+
+// full Christmas tree
+function drawTree2(h) {
+	for (i = 0, k = h; i < h; i=i+2, k=k-2) {
+		var star = '*';
+		var space = '';
+		for (j = 0; j < i; j=j+2) {
+				star += '**';
+		}
+		for (m = 1; m < k/2+0.5; m++) {
+			space += ' ';
+		}
+		console.log(space+star);
+	}
+}
+
+drawTree2(17);
+
+// nautural Christmas tree (not so pretty but original)
+function drawTree3(h) {
 	for (i = 1, k = h; i <= h; i++, k--) {
 		var star = '';
 		var space = '';
@@ -14,4 +45,4 @@ function drawTree(h) {
 	}
 }
 
-drawTree(9);
+drawTree3(9);
