df6496e06b15906df467412dd4e2fafeb12adf58
Pawel Kowaliszyn <pkowaliszyn@onet.eu>
2017-12-03 11:52:52 +0100

    Update in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
 // scripts.js
 
-var i;
-var j;
-var k;
-var m;
-
 // half Chrismas tree
 function drawTree(h) {
-	for (i = 1; i <= h; i++) {
+	for (var i = 1; i <= h; i++) {
 		var star = '';
-		for (j = 0; j < i; j++) {
+		for (var j = 0; j < i; j++) {
 			star += '*';
 		}
 		console.log(star);
@@ -20,13 +15,13 @@ drawTree(9);
 
 // full Christmas tree
 function drawTree2(h) {
-	for (i = 0, k = h; i < h; i = i+2, k = k-2) {
+	for (var i = 0, k = h; i < h; i = i+2, k = k-2) {
 		var star = '*';
 		var space = '';
-		for (j = 0; j < i; j = j+2) {
+		for (var j = 0; j < i; j = j+2) {
 				star += '**';
 		}
-		for (m = 1; m < k/2 + 0.5; m++) {
+		for (var m = 1; m < k/2 + 0.5; m++) {
 			space += ' ';
 		}
 		console.log(space+star);
@@ -37,13 +32,13 @@ drawTree2(17);
 
 // nautural Christmas tree (not so pretty but original)
 function drawTree3(h) {
-	for (i = 1, k = h; i <= h; i++, k--) {
+	for (var i = 1, k = h; i <= h; i++, k--) {
 		var star = '';
 		var space = '';
-		for (j = 0; j < i; j++) {
+		for (var j = 0; j < i; j++) {
 				star += '*';
 		}
-		for (m = 1; m < k/2; m++) {
+		for (var m = 1; m < k/2; m++) {
 			space += ' ';
 		}
 		console.log(space+star);
